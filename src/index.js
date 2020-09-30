800124bf4f74b7097f6b76519286074b55b88dda
jungRoit <roit.github@gmail.com>
2020-09-30 23:34:40 +0545

    updated port

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import router from './routes';
 import { genericErrorHandler } from './middlewares/errorHandler';
 
 const app = express();
-const port = 9090;
+const port = process.env.PORT || 9090;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
